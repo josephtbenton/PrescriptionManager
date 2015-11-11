add2cd28c721827113083503b41f98f8016d9234
GerryJarcia <tschiemerbr@hendrix.edu>
2015-11-10 20:17:40 -0600

    Update DrugRepositoryTest.java

--- src/PrescriptionManager/DrugRepositoryTest.java
+++ src/PrescriptionManager/DrugRepositoryTest.java
@@ -21,6 +21,21 @@ public class DrugRepositoryTest {
 	}
 	
 	@Test
+	public void testGetDrugID() throws ClassNotFoundException, SQLException {
+		DrugRepository drugRepo;
+		drugRepo = new DrugRepository();
+		drugRepo.addDrug("Lisinopril", "10 mg", 300);
+		drugRepo.addDrug("Azithromycin", "200 mg", 2000);
+		drugRepo.addDrug("Amoxicillin", "300 mg", 2000);
+		assertTrue(drugRepo.getDrugID("Lisinopril", "10 mg") == 1);
+		assertTrue(drugRepo.getDrugID("Azithromycin", "200 mg") == 2);
+		assertTrue(drugRepo.getDrugID("Amoxicillin", "300 mg") == 3);
+		assertFalse(drugRepo.getDrugID("Lisinopril", "10 mg") == 2);
+		assertFalse(drugRepo.getDrugID("Azithromycin", "200 mg") == 4);
+		assertFalse(drugRepo.getDrugID("Amoxicillin", "300 mg") == 1);
+	}
+	
+	@Test
 	public void testRemoveDrug() throws ClassNotFoundException, SQLException {
 		DrugRepository drugRepo;
 		drugRepo = new DrugRepository();
@@ -45,4 +60,51 @@ public class DrugRepositoryTest {
 		assertTrue(drugRepo.getCount(drugRepo.getDrugID("Amoxicillin", "300 mg")) == 2000);
 	}
 	
+	@Test
+	public void testUpdateCount() throws ClassNotFoundException, SQLException {
+		DrugRepository drugRepo;
+		drugRepo = new DrugRepository();
+		drugRepo.addDrug("Lisinopril", "10 mg", 300);
+		drugRepo.addDrug("Azithromycin", "200 mg", 2000);
+		drugRepo.addDrug("Amoxicillin", "300 mg", 2000);
+		drugRepo.updateCount(drugRepo.getDrugID("Lisinopril", "10 mg"), 700);
+		drugRepo.updateCount(drugRepo.getDrugID("Azithromycin", "200 mg"), 3000);
+		drugRepo.updateCount(drugRepo.getDrugID("Amoxicillin", "300 mg"), 500);
+		assertTrue(drugRepo.getCount(drugRepo.getDrugID("Lisinopril", "10 mg")) == 1000);
+		assertTrue(drugRepo.getCount(drugRepo.getDrugID("Azithromycin", "200 mg")) == 5000);
+		assertTrue(drugRepo.getCount(drugRepo.getDrugID("Amoxicillin", "300 mg")) == 2500);
+		assertFalse(drugRepo.getCount(drugRepo.getDrugID("Amoxicillin", "300 mg")) == 500);
+		drugRepo.updateCount(drugRepo.getDrugID("Azithromycin", "200 mg"), -1000);
+		assertTrue(drugRepo.getCount(drugRepo.getDrugID("Azithromycin", "200 mg")) == 4000);
+	}
+	
+	@Test
+	public void testGetStrength() throws ClassNotFoundException, SQLException {
+		DrugRepository drugRepo;
+		drugRepo = new DrugRepository();
+		drugRepo.addDrug("Lisinopril", "10 mg", 300);
+		drugRepo.addDrug("Azithromycin", "200 mg", 2000);
+		drugRepo.addDrug("Amoxicillin", "300 mg", 2000);
+		assertTrue(drugRepo.getStrength(drugRepo.getDrugID("Lisinopril", "10 mg")) == "10 mg");
+		assertTrue(drugRepo.getStrength(drugRepo.getDrugID("Azithromycin", "200 mg")) == "200 mg");
+		assertTrue(drugRepo.getStrength(drugRepo.getDrugID("Amoxicillin", "300 mg")) == "300 mg");
+		assertFalse(drugRepo.getStrength(drugRepo.getDrugID("Lisinopril", "10 mg")) == "100000 mg");
+		assertFalse(drugRepo.getStrength(drugRepo.getDrugID("Azithromycin", "200 mg")) == "1 mg");
+		assertFalse(drugRepo.getStrength(drugRepo.getDrugID("Amoxicillin", "300 mg")) == "12 kg");
+	}
+	
+	@Test
+	public void testGetDrug() throws ClassNotFoundException, SQLException {
+		DrugRepository drugRepo;
+		drugRepo = new DrugRepository();
+		drugRepo.addDrug("Lisinopril", "10 mg", 300);
+		drugRepo.addDrug("Azithromycin", "200 mg", 2000);
+		drugRepo.addDrug("Amoxicillin", "300 mg", 2000);
+		assertTrue(drugRepo.getDrug(drugRepo.getDrugID("Lisinopril", "10 mg")) == "Lisinopril");
+		assertTrue(drugRepo.getDrug(drugRepo.getDrugID("Azithromycin", "200 mg")) == "Azithromycin");
+		assertTrue(drugRepo.getDrug(drugRepo.getDrugID("Amoxicillin", "300 mg")) == "Amoxicillin");
+		assertFalse(drugRepo.getDrug(drugRepo.getDrugID("Lisinopril", "10 mg")) == "Azithromycin");
+		assertFalse(drugRepo.getDrug(drugRepo.getDrugID("Azithromycin", "200 mg")) == "Amoxicillin");
+		assertFalse(drugRepo.getDrug(drugRepo.getDrugID("Amoxicillin", "300 mg")) == "Lisinopril");
+	}
 }
