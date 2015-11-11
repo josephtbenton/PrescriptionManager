4e181bfc90d3407f1f42e9188b9d1058d640c1c4
josephtbenton <josephtbenton@gmail.com>
2015-11-10 22:07:06 -0600

    I think things work besides dob

--- src/PrescriptionManager/DrugRepository.java
+++ src/PrescriptionManager/DrugRepository.java
@@ -5,63 +5,87 @@ import java.sql.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 public class DrugRepository {
-    private Statement stat;
-    private Connection con;
     private ObservableList<String> drugList;
 
 
     //constructor creates a table if one does not exist else just initializes connection to sqlite
     public DrugRepository() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
-        con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
-        stat = con.createStatement();
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
         this.drugList = FXCollections.observableArrayList();
         String create = "CREATE TABLE IF NOT EXISTS Inventory (Drug_ID INT, Name CHAR(255), Strength CHAR(255), Count INT)";
         stat.execute(create);
+        con.close();
     }
 
     //updates the count of a given drug id
     public void updateCount(int ID, int update) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
         int count = getCount(ID);
         count = count + update;
         String updateCount = "UPDATE Inventory SET Count=" + count + "WHERE Drug_ID=" + ID;
         stat.execute(updateCount);
+        con.close();
 
     }
 
     //gets the count of a given drug id
     public int getCount(int ID) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
         String counts = "SELECT Count FROM Inventory WHERE Drug_ID=" + ID;
         stat.execute(counts);
         ResultSet count = stat.getResultSet();
-        return count.getInt(1);
+        int res = count.getInt(1);
+        con.close();
+        count.close();
+        return res;
     }
 
     public String getStrength(int ID) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
         String counts = "SELECT Strength FROM Inventory WHERE Drug_ID=" + ID;
         stat.execute(counts);
         ResultSet count = stat.getResultSet();
-        return count.getString(1);
+        String res = count.getString(1);
+        con.close();
+        count.close();
+        return res;
     }
 
     //gets the string name of a certin drug id
     public String getDrug(int ID) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
         String drug = "SELECT Drugs FROM Inventory WHERE Drug_ID=" + ID;
         stat.execute(drug);
         ResultSet drugs = stat.getResultSet();
-        return drugs.getNString(1);
+        String res = drugs.getNString(1);
+        con.close();
+        drugs.close();
+        return res;
     }
 
     //gets the drug id of a given drug name
     public int getDrugID(String name , String strength) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
         String drugID = "SELECT Drug_ID FROM Inventory WHERE Name = \'" + name + "\' AND Strength = \'" + strength + "\'" ;
         stat.execute(drugID);
         ResultSet result = stat.getResultSet();
-        return result.getInt(1);
+        int res = result.getInt(1);
+        con.close();
+        result.close();
+        return res;
     }
 
     //adds a drug to Inventory.. first part increments drug id
     public void addDrug(String name, String strength, int count) throws SQLException{
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
         drugList.add(name + " - " + strength);
         String getLength = "SELECT count(*) AS length FROM Inventory";
         stat.execute(getLength);
@@ -70,24 +94,34 @@ public class DrugRepository {
         length +=1;
         String insert = "INSERT INTO Inventory VALUES (" + length + ", \'" + name + "\', \'" + strength + "\', " + count + ")";
         stat.execute(insert);
+        con.close();
+        Length.close();
     }
 
     //removes a drug given a drug Id
     public void removeDrug(int ID) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
         String remove = "DELETE FROM Inventory WHERE Drug_ID=" +ID;
         stat.execute(remove);
+        con.close();
     }
 
 
     //ONLY CALL THIS ONCE in the CONTROllER so far since it adds to the list... will fix
     public ObservableList<String> getDrugList() throws SQLException {
-        String dugs = "SELECT Name, Strength FROM Inventory";
-        stat.execute(dugs);
-        ResultSet drugs = stat.getResultSet();
-        while (drugs.next()) {
-            drugList.add(drugs.getString(1) + " - " + drugs.getString(2));
+        if (drugList.isEmpty()) {
+            Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+            Statement stat = con.createStatement();
+            String dugs = "SELECT Name, Strength FROM Inventory";
+            stat.execute(dugs);
+            ResultSet drugs = stat.getResultSet();
+            while (drugs.next()) {
+                drugList.add(drugs.getString(1) + " - " + drugs.getString(2));
+            }
+            con.close();
+            drugs.close();
         }
-
         return drugList;
     }
 
--- src/PrescriptionManager/PatientRepository.java
+++ src/PrescriptionManager/PatientRepository.java
@@ -6,38 +6,45 @@ import javafx.collections.ObservableList;
 import java.sql.*;
 
 public class PatientRepository {
-    private Statement stat;
-    private Connection con;
     private ObservableList<String> patientList;
 
 
     //constructor creates a table if one does not exist else just initializes connection to sqlite
     public PatientRepository() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
-        con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
-        stat = con.createStatement();
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
         this.patientList = FXCollections.observableArrayList();
         String create = "CREATE TABLE IF NOT EXISTS patients (pid INT, firstname CHAR(255), lastname CHAR(255), dob CHAR(255))";
         stat.execute(create);
+        con.close();
     }
 
     //gets the drug id of a given drug name
     public int getPID(String firstName, String lastName, String dob) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
         String drugID = "SELECT pid FROM patients WHERE Name = \'" + firstName + "\' AND lastname = \'" + lastName + "\' AND dob = \'" + dob + "\'";
         stat.execute(drugID);
         ResultSet drugid = stat.getResultSet();
+        con.close();
         return drugid.getInt(1);
     }
     
     public String getDOB(int Pid) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
         String DOB = "SELECT dob FROM patients WHERE pid = " + Pid;
         stat.execute(DOB);
         ResultSet drugid = stat.getResultSet();
+        con.close();
         return drugid.getString(1);
     }
 
     //adds a drug to Inventory.. first part increments drug id
     public void addPatient(String firstName, String lastName, String dob) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
         String lengthOf = "SELECT count(*) AS length FROM patients";
         stat.execute(lengthOf);
         ResultSet Length = stat.getResultSet();
@@ -46,21 +53,23 @@ public class PatientRepository {
         String insert = "INSERT INTO patients VALUES (" + length + ", \'" + firstName + "\', \'" + lastName + "\', \'" + dob + "\')";
         stat.execute(insert);
         patientList.add(lastName + ", " + firstName + " (" + length + ")");
+        con.close();
     }
 
     //removes a drug given a drug Id
     public void removePatient(int ID) throws SQLException {
-<<<<<<< HEAD
-        String remove = "delete from patients where pid=" + ID;
-=======
-        String remove = "DELETE FROM patients WHERE Drug_ID=" + ID;
->>>>>>> 6952fba89a84c8cf22be36492b96ad2075172562
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
+        String remove = "DELETE FROM patients WHERE pid=" + ID;
         stat.execute(remove);
+        con.close();
     }
 
 
     //ONLY CALL THIS ONCE in the CONTROllER so far since it adds to the list... will fix
     public ObservableList<String> getPatientList() throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:sqlite:prescriptions.db");
+        Statement stat = con.createStatement();
     	if (patientList.isEmpty()) {
 	        String pats = "SELECT firstname, lastname, pid FROM patients";
 	        stat.execute(pats);
@@ -69,6 +78,7 @@ public class PatientRepository {
 	            patientList.add(patients.getString(2) +  ", " + patients.getString(1) + " (" + patients.getInt(3) + ")");
 	        }
     	}
+        con.close();
         return patientList;
     }
 
